58da02f3e3b5a1723203c4883ada6eaf7fe37ff5
blueprint-12 <blueprint12@daum.net>
2022-04-20 21:54:47 +0900

    백준문제: 개 출력하기

---
+++ 백준문제/입출력문제/dog.js
@@ -0,0 +1,8 @@
+//🍕백준문제10172번 개 만들기
+//key point: String 이스케이프 표현 - 백슬래쉬\
+
+console.log("|\\_/|");
+console.log("|q p|   /}");
+console.log('( 0 )"""\\');
+console.log('|"^"`    |');
+console.log("||_/=\\\\__|");
